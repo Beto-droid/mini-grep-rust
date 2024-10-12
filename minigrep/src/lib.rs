f3353e22866d1db78d72c94324a904d886b8d07f
Beto-droid <alberto.javier.magrini@gmail.com>
2024-10-11 21:31:58 -0300

    innit

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,54 @@
+use std::error::Error;
+use std::fs;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents =
+        fs::read_to_string(config.file_path)?;
+    for line in search(&config.query, &contents) {
+        println!("{}", {line});
+    }
+    Ok(())
+}
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        Ok(Config {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for lines in contents.lines() {
+        if lines.contains(query) {
+            results.push(lines.trim())
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
